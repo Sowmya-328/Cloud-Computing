cec29b252bd2ed0acd0b106d73dc6744e837905e
Sowmya-328 <230701328@rajalakshmi.edu.in>
2026-04-19 23:33:06 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:5000/api',
+    baseURL: 'https://workpulse-backend-bagchcfsh6h5dxbz.eastasia-01.azurewebsites.net/api',
 });
 
 api.interceptors.request.use((config) => {
